c16e7fb54f5186560893ec17d37cdf3c05f96f9f
Sergey Markin <sergey.markin@fastdev.se>
2022-05-07 18:17:04 +0400

    Add project files.

---
+++ 7 Refactoring/Database.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _7_Refactoring
+{
+    public class Database
+    {
+        internal object[] GetUserById(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object[] GetCompany()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void SaveCompany(int newNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void SaveUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 7 Refactoring/MessageBus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _7_Refactoring
+{
+    internal class MessageBus
+    {
+        internal void SendEmailChangedMessage(int userId, string newEmail)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
